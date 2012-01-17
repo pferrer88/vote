fdc17d5e8079e44bdde6bdc1b12909446086c6cf
Pedro F <pferrer88@gmail.com>
2012-01-17 15:41:20 -0430

    no

--- config/application.rb
+++ config/application.rb
@@ -61,7 +61,7 @@ module Voto
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     
-    config.assets.paths << Rails.root.join("vendor", "assets", "stylesheets")
+    #config.assets.paths << Rails.root.join("vendor", "assets", "stylesheets")
     config.assets.paths << Rails.root.join("vendor", "assets", "javascripts")
   end
 end
