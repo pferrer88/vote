e055209d5ae056f60be0ad535b38082d9a85a8f7
Pedro F <pferrer88@gmail.com>
2012-01-26 17:04:06 -0430

    finally mail is working with aws ses

--- config/initializers/gmail_smtp.rb
+++
@@ -1,8 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => 'embajadordelvoto.org',
-  :user_name            => 'embajador',
-  :password             => 'schloeter',
-  :authentication       => 'plain',
-  :enable_starttls_auto => true  }
