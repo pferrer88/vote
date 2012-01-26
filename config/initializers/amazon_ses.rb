5103c7bb57142d167e37614a1e833c002d99e320
Pedro F <pferrer88@gmail.com>
2012-01-26 16:05:47 -0430

    changed amazon credentials

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,3 +1,3 @@
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
-  :access_key_id     => ENV['AKIAJROYHH2JACZ4HJZA'],
-  :secret_access_key => ENV['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']
+  :access_key_id     => 'AKIAJROYHH2JACZ4HJZA',
+  :secret_access_key => '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'
