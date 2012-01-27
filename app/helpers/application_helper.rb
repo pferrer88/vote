1abbc748caa8c42db55abe2b78ce24c7e9402cc8
Pedro F <pferrer88@gmail.com>
2012-01-26 22:46:11 -0430

    cambios de tituos y small fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,6 +27,14 @@ module ApplicationHelper
     end
   end
   
+  # def page_title(title=view_context.t('.title'))
+  #     content_tag(:h1, title)
+  # end
+  
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+
   # def devise_error_messages!
   #     return "" if resource.errors.empty?
   # 
