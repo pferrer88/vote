45e2fc8bd31e66b13a4a8e3af412bc25b7c89090
Pedro F <pferrer88@gmail.com>
2012-02-02 13:23:11 -0430

    changed home text and thermometer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,8 +6,8 @@ class HomeController < ApplicationController
       @invites = User.invitado_por(current_user).page(params[:invite_page]).per(5)
       @exchanges = Exchange.comprado_por(current_user).page(params[:exchange_page]).per(5)
     else
-      @invited_count = User.count
-      @confirmed_count = User.confirmado.count
+      @registered_count = User.registrado.count
+      #@confirmed_count = User.confirmado.count
       @embajador_count = User.embajador.count
     end  
   end
