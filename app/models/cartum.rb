8c068c7079da5a468568e98b752eece38765f7b6
Pedro F <pferrer88@gmail.com>
2012-02-14 06:54:30 -0430

    accesors for attribute

--- app/models/cartum.rb
+++ app/models/cartum.rb
@@ -2,5 +2,5 @@ class Cartum < ActiveRecord::Base
   has_many :signatures
   scope :activo, where(:active => true).limit(1)
   
-  attr_accessible :content, :title, :cartum_id
+  attr_accessible :content, :title, :cartum_id, :name, :active
 end
